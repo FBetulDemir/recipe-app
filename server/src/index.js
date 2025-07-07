68c4529d476bb54bd848b248c62a31df68214c01
FBetulDemir <fatmabetuld@gmail.com>
2025-07-07 18:22:45 +0200

    Added model and routes for user registeration

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+import { userRouter } from './routes/users.js';
+
+dotenv.config();
 
 
 // const express = require('express');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use("/auth", userRouter);
+
+mongoose.connect(process.env.MONGO_URI)
 
 app.listen(3001, () =>{"Server started!"})
