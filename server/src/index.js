8702b9f16426b057524eb433edd9d20e12c37ecc
FBetulDemir <fatmabetuld@gmail.com>
2025-08-11 09:50:24 +0200

    Configure CORS to allow requests from the specified origin and enable credentials

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,11 @@ dotenv.config();
 // const express = require('express');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://herb-heat.netlify.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/auth", userRouter);
